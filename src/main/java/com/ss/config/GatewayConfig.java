072b358e63ec48884b908ff3f475b857a45e07ea
imsadanala <ssadanala@deloitte.com>
2021-03-03 19:59:12 +0530

    added routes to exchange & conversion services

--- src/main/java/com/ss/config/GatewayConfig.java
+++ src/main/java/com/ss/config/GatewayConfig.java
@@ -17,6 +17,10 @@ public class GatewayConfig {
 								.addRequestHeader("myheader", "myheader")
 								.addRequestParameter("myparam", "myparam"))
 						.uri("http://httpbin.org:80"))
+				.route(p -> p.path("/currency-exchange/**")
+						.uri("lb://currency-exchange"))
+				.route(p -> p.path("/currency-conversion/**")
+						.uri("lb://currency-conversion"))
 				.build();
 	}
 
